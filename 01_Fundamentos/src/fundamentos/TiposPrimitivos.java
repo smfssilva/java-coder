d844097c53ead3e423052acbf4a72219fbaa68a7
smfssilva <smfssilva@yahoo.com>
2022-04-02 01:46:21 -0300

    Tipos primitivos

---
+++ 01_Fundamentos/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+  public static void main(String[] args) {
+    System.out.println();
+
+    // Tipos numericos inteiros
+    byte anosDeEmpresa = 23;
+    short numeroDeVoos = 542;
+    int id = 56789;
+    long pontosAcumulados = 3_234_845_223L;
+
+    // Tipos numéricos reais
+    float salario = 11_445.44F;
+    double vendasAcumuladas = 2_991_797_103.01;
+
+    // Tipos booleano
+    boolean estaDeFerias = false;
+
+    // Tipo caracter
+    char status = 'A';
+
+    System.out.println(status);
+
+    System.out.println();
+  }
+}
