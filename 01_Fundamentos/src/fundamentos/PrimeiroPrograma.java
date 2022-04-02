ddde61c93b1f6770b7848589665ddb6cdc575dfc
smfssilva <smfssilva@yahoo.com>
2022-04-02 00:12:08 -0300

    Comentarios

--- 01_Fundamentos/src/fundamentos/PrimeiroPrograma.java
+++ 01_Fundamentos/src/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,17 @@ package fundamentos;
 public class PrimeiroPrograma {
 
   public static void main(String[] args) {
+
+    // Comentario de uma linha
+    /*
+      Comentario de
+      multiplas linhas
+    */
+
+    /**
+     * @author Sivanilson
+     */
+
     System.out.println("Hello world!");
   }
 }
