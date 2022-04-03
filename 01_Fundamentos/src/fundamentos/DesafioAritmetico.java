aee4f4a8a3c204cd3b150ce86a78ce584412635f
smfssilva <smfssilva@yahoo.com>
2022-04-03 16:28:27 -0300

    Desafio Aritmeticos

---
+++ 01_Fundamentos/src/fundamentos/DesafioAritmetico.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class DesafioAritmetico {
+
+  public static void main(String[] args) {
+
+    double numA = Math.pow(6 * (3 + 2), 2);
+    double denA = 3 * 2;
+    double resA = numA / denA;
+
+    double numB = Math.pow((1 - 5) * (2 - 7), 2) / 2;
+    double denB = 2;
+    double resB = numB / denB;
+
+    double resultado = Math.pow(resA - resB, 3) / Math.pow(10, 3);
+
+
+    System.out.println("O resultado é: " + resultado);
+
+  }
+}
