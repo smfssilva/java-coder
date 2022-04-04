cece334cc5e61ee02ce3e8a5838ccdf59c057646
smfssilva <smfssilva@yahoo.com>
2022-04-04 11:49:25 -0300

    Desafio Construtores

--- 01_Fundamentos/src/Classe/Data.java
+++ 01_Fundamentos/src/Classe/Data.java
@@ -5,6 +5,18 @@ public class Data {
   int mes;
   int ano;
 
+  public Data() {
+    dia = 1;
+    mes = 1;
+    ano = 1970;
+  }
+
+  public Data(int dia, int mes, int ano) {
+    this.dia = dia;
+    this.mes = mes;
+    this.ano = ano;
+  }
+
   String obterDataFormatada(){
     return String.format("%02d/%02d/%d", dia, mes, ano);
   }
--- 01_Fundamentos/src/Classe/DataTeste.java
+++ 01_Fundamentos/src/Classe/DataTeste.java
@@ -2,15 +2,15 @@ package Classe;
 
 public class DataTeste {
   public static void main(String[] args) {
-    Data d1 = new Data();
-    d1.dia = 24;
-    d1.mes = 7;
-    d1.ano = 1977;
+    Data d1 = new Data(24, 7, 1977);
+//    d1.dia = 24;
+//    d1.mes = 7;
+//    d1.ano = 1977;
 
     var d2 = new Data();
-    d2.dia = 13;
-    d2.mes = 8;
-    d2.ano = 1979;
+//    d2.dia = 13;
+//    d2.mes = 8;
+//    d2.ano = 1979;
 
     System.out.printf("%d/%02d/%d\n", d1.dia, d1.mes, d1.ano);
     System.out.printf("%d/%02d/%d\n", d2.dia, d2.mes, d2.ano);
