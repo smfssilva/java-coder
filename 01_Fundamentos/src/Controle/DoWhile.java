9711b44aa0f0b381afa9af93b3af4ea6c562b899
smfssilva <smfssilva@yahoo.com>
2022-04-03 21:36:39 -0300

    DoWhile

---
+++ 01_Fundamentos/src/Controle/DoWhile.java
@@ -0,0 +1,10 @@
+package Controle;
+
+public class DoWhile {
+  public static void main(String[] args) {
+    int nota = 10;
+    do {
+      System.out.println("Passou!!!");
+    } while (nota != 10);
+  }
+}
