206b179e9d0c637ff4a434796435b43c6120be6f
smfssilva <smfssilva@yahoo.com>
2022-04-04 11:44:20 -0300

    Construtores

--- 01_Fundamentos/src/Classe/Produto.java
+++ 01_Fundamentos/src/Classe/Produto.java
@@ -5,6 +5,15 @@ public class Produto {
   double preco;
   double desconto;
 
+  public Produto() {
+  }
+
+  public Produto(String nome, double preco, double desconto) {
+    this.nome = nome;
+    this.preco = preco;
+    this.desconto = desconto;
+  }
+
   double precoComDesconto() {
     return preco * (1 - desconto);
   }
--- 01_Fundamentos/src/Classe/ProdutoTeste.java
+++ 01_Fundamentos/src/Classe/ProdutoTeste.java
@@ -2,11 +2,11 @@ package Classe;
 
 public class ProdutoTeste {
   public static void main(String[] args) {
-    Produto p1 = new Produto();
-    p1.nome = "Notebook";
-    p1.preco = 4500;
-    p1.desconto = .1;
+    Produto p1 = new Produto("Notebook",4500,0.1);
+//    p1.nome = "Notebook";
+//    p1.preco = ;
+//    p1.desconto = .1;
     System.out.printf("%.2f\n", p1.precoComDesconto());
-    System.out.printf("Valor com desconto: R$ %.2f\n", p1.precoComDesconto(.1));
+    System.out.printf("%s Valor com desconto: R$ %.2f\n",p1.nome, p1.precoComDesconto(.1));
   }
 }
