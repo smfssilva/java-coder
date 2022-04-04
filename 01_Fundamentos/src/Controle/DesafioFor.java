3847fa9b293aab7060c9dabfcfc71d3685721875
smfssilva <smfssilva@yahoo.com>
2022-04-03 22:17:39 -0300

    Desafio For

---
+++ 01_Fundamentos/src/Controle/DesafioFor.java
@@ -0,0 +1,10 @@
+package Controle;
+
+public class DesafioFor {
+  public static void main(String[] args) {
+    String valor = "#";
+    for (String v = "#"; !v.equals("######"); v += "#") {
+      System.out.println(v);
+    }
+  }
+}
