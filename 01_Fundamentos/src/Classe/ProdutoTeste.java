22b2a9b9fcd2526de18faf61c3285623c06e5f25
smfssilva <smfssilva@yahoo.com>
2022-04-04 00:42:22 -0300

    Metodos

--- 01_Fundamentos/src/Classe/ProdutoTeste.java
+++ 01_Fundamentos/src/Classe/ProdutoTeste.java
@@ -4,8 +4,9 @@ public class ProdutoTeste {
   public static void main(String[] args) {
     Produto p1 = new Produto();
     p1.nome = "Notebook";
-    p1.preco = 4356.89;
-    p1.desconto = .25;
-    System.out.printf("%.2f\n", p1.preco * (1 - p1.desconto));
+    p1.preco = 4500;
+    p1.desconto = .1;
+    System.out.printf("%.2f\n", p1.precoComDesconto());
+    System.out.printf("Valor com desconto: R$ %.2f\n", p1.precoComDesconto(.1));
   }
 }
