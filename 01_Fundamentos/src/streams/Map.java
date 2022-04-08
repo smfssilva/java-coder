9a21554357e090d9c27221cb72a1beeb4ae2c701
smfssilva <smfssilva@yahoo.com>
2022-04-08 20:18:01 -0300

    Stream com Map

---
+++ 01_Fundamentos/src/streams/Map.java
@@ -0,0 +1,36 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Map {
+  public static void main(String[] args) {
+
+    Consumer<String> print = System.out::print;
+
+    List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda ");
+
+    //Sem metodo referencia
+    marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+    // Com metodo referencia
+    marcas.stream().map(String::toUpperCase).forEach(print);
+
+    UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+    UnaryOperator<String> primeriaLetra = n -> n.charAt(0)+"";
+    UnaryOperator<String> grito = n -> n + "!!!";
+
+    String result = maiuscula.andThen(primeriaLetra).andThen(grito).apply(marcas.get(0));
+    System.out.println(result);
+
+    // usando streams
+    marcas.stream()
+            .map(maiuscula)
+            .map(primeriaLetra)
+            .map(grito)
+            .forEach(print);
+
+
+  }
+}
